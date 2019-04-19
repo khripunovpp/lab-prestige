9b8ec3069563c41ac99795857e4b7691d1f003d1
Павел Хрипунов <khripunovpp@gmail.com>
2019-04-19 15:24:32 +0300

    basket

--- app/js/basket.js
+++ app/js/basket.js
@@ -1,7 +1,44 @@
+var ajax = function(form, attach) {
+
+    var formtarget = form,
+        msg = { userData: $(formtarget).serializeArray(), basket: attach },
+        jqxhr = $.post("/ajax.php", msg, onAjaxSuccess);
+
+    function onAjaxSuccess(data) {
+
+        var json = JSON.parse(data),
+            status = json.status,
+            message = json.message,
+            formid = json.form;
+
+        if (status === 'success') {
+            $('input, textarea, button[type=submit]').each(function() {
+                $(this).prop("disabled", "true");
+            });
+        }
+
+        addNotify(status, message, formid)
+    }
+
+    var addNotify = function(status, msg, form) {
+        var responsePopup = status === 'error' ? $('.response--error') : $('.response--success')
+
+        responsePopup.fadeIn();
+        $('.modal').fadeOut();
+        $('.popup').fadeOut();
+
+        setTimeout(function() {
+            responsePopup.fadeOut();
+        }, 2000)
+    }
+}
+
 var basket = {
     state: {},
     init: function() {
         var _that = this
+
+        _that.basket = $('.basket')
 
         _that._update()
 
@@ -10,13 +47,15 @@ var basket = {
             $(this).closest('.product').addClass('added')
         })
 
-        $('.product__delete').on('click', function(e) {
+        $('body').on('click', '.product__delete, .basket__item-delete', function(e) {
             _that._removeItem.call(_that, e)
         });
 
         $('.js-clear').on('click', function(e) {
             _that._clearBasket.call(_that, e)
         });
+
+        _that._steps()
     },
     _add: function(event) {
         var _that = this
@@ -57,7 +96,7 @@ var basket = {
     _removeItem: function(event) {
         var _that = this
 
-        var id = $(event.target).closest('.product').attr('data-id')
+        var id = $(event.target).closest('.product, .basket__item').attr('data-id')
 
         var basket = _that._getBasket(),
             basketFinal = [];
@@ -65,6 +104,8 @@ var basket = {
         basket.map(function(elem, i) {
             if (elem.id !== id) basketFinal.push(elem)
         })
+
+        $(event.target).closest('.basket__item').fadeOut()
 
         $.cookie('basket', JSON.stringify(basketFinal));
 
@@ -76,11 +117,14 @@ var basket = {
         var totalPrice = 0
 
         $('.product').removeClass('added')
+        $('.basket__list').html('')
 
-        _that._getBasket().map(function(elem) {
+        var basket = _that._getBasket()
+
+        basket.map(function(elem) {
             $('[data-id="' + elem.id + '"]').addClass('added')
 
-            var prodItem = $('<div class="basket__item" data-id="' + elem.id + '"><div class="basket__item-info"> <button class="basket__item-delete" title="Удалить из корзины"></button></div></div>')
+            var prodItem = $('<div class="basket__item" data-id="' + elem.id + '"><div class="basket__item-info"> <button class="basket__item-delete" type="button">Удалить</button></div></div>')
             if (elem.thumb) prodItem.prepend('<div class="basket__item-pic"><img src="' + elem.thumb + '" alt="" /></div>')
             prodItem.find('.basket__item-info').append('<p class="basket__item-name">' + elem.name + '</p>')
             prodItem.find('.basket__item-info').append('<p class="basket__item-costType">Тип цены: <strong>' + elem.costType + '</strong></p>')
@@ -92,7 +136,81 @@ var basket = {
             $('.basket__list').append(prodItem)
         })
 
+        basket.length <= 0 ? _that.basket.addClass('empty') : _that.basket.removeClass('empty')
+
         $('.js-basketTotal').text(totalPrice)
+    },
+    _steps: function() {
+        var _that = this
+
+        var inputs = $('input, textarea').filter('[required]')
+
+        var activeStep, errors = 0
+
+        setLocation()
+
+        $(inputs).on('blur', function() {
+            removeError($(event.target))
+        });
+
+        $('[type="radio"]').on('click', function() {
+            $(this).closest('.basket__group-radio').addClass('checked').siblings().removeClass('checked')
+        });
+
+        $('.basket__next').on('click', function(event) {
+            event.preventDefault();
+
+            inputs = $('input, textarea', $(event.target).closest('.basket__step-body')).filter('[required]')
+
+            inputs.each(function(index, el) {
+                $(el).val().length === 0 && setError($(el).closest('.basket__group'))
+            });
+
+            nextStep($(event.target).closest('.basket__step'))
+        });
+
+        $('.basket__ok').on('click', submit)
+
+        function setError(el) {
+            if (!el.hasClass('invalid')) el.addClass('invalid').append('<p class="basket__group-error">Это обязательное поле</p>'), errors++
+        }
+
+        function removeError(el) {
+            if (el.val().length > 0) el.closest('.basket__group').removeClass('invalid').find('.basket__group-error').remove(), errors--
+        }
+
+        function nextStep(prev) {
+            activeStep = $(prev)
+
+            if (errors <= 0) activeStep.addClass('disabled done').next().removeClass('disabled'), errors = 0
+        }
+
+        function submit() {
+            var basket = _that._getBasket()
+
+            if (basket.length > 0) {
+                ajax(_that.basket, basket)
+
+                ////
+
+                $('.response--success').fadeIn(100);
+            } else {
+                _that.basket.addClass('error')
+            }
+        }
+
+        function setLocation() {
+            if ("geolocation" in navigator) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    $.ajax({
+                        url: 'https://geocode-maps.yandex.ru/1.x/?geocode=' + position.coords.longitude + ',' + position.coords.latitude,
+                        success: function(data) {
+                            $('[name="location"]').val($(data).find('featureMember').eq(0).find('description').text())
+                        }
+                    });
+                });
+            }
+        }
     }
 }
 
